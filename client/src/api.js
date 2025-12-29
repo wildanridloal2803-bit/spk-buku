9a80755547bb3ca97e2690e3db42dc80ca22bbef
wildanridloal2803-bit <wildanridloal2803@gmail.com>
2025-12-29 09:52:12 +0700

    fiks

--- client/src/api.js
+++ client/src/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 
 const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+console.log("🔗 Frontend nembak ke:", baseURL);
 // Ganti baseURL sesuai port backend Node.js kamu
 const api = axios.create({
     baseURL: baseURL,
--- server.js
+++ server.js
@@ -16,7 +16,8 @@ const PORT = process.env.PORT || 5000;
 // Kalau port Vite kamu beda, sesuaikan di sini.
 app.use(cors({
     origin: 'http://localhost:5173', 
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
 // Agar backend bisa membaca data JSON yang dikirim dari Frontend
